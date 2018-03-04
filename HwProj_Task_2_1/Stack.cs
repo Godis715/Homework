9ffb8d7cdbb9042562dbafcc165c3142ebc66d39
Denis Ignatev <godis7.15@gmail.com>
2018-03-04 14:58:30 +0300

    Task 2_1 gave been done

---
+++ HwProj_Task_2_1/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HwProj_Task_2_1
+{
+	public class Stack
+	{
+		private class Node
+		{
+			public Node Prev { get; set; }
+			public int Value { get; set; }
+
+			public Node(int value) {
+				Prev = null;
+				Value = value;
+			}
+		}
+
+		private Node head;
+		private int size;
+		public int Size
+		{
+			get { return size; }
+		}
+
+		public Stack() {
+			head = null;
+			size = 0;
+		}
+
+		public void Push(int value)
+		{
+			Node newNode = new Node(value);
+			newNode.Prev = head;
+			head = newNode;
+			++size;
+		}
+
+		public int Pop() {
+			if (Size == 0) {
+				throw new IndexOutOfRangeException();
+			}
+			Node temp = head;
+			head = head.Prev;
+			--size;
+			return temp.Value;
+		}
+
+	}
+}
