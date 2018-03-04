716d38df4e2ed8e2dd6396d95a1923788cae0bf6
Denis Ignatev <godis7.15@gmail.com>
2018-03-04 14:54:53 +0300

    Task 2_1 have been done

---
+++ HwProj_Task_2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HwProj_Task_2_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack stack = new Stack();
+			stack.Push(5);
+			stack.Pop();
+		}
+	}
+}
