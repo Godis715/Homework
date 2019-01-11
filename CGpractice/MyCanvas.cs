48f2d933db859c72625b629b50194434659c2ff5
Denis Ignatev <godis7.15@gmail.com>
2019-01-12 02:46:47 +0300

    MyCanvas class

---
+++ CGpractice/MyCanvas.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace CGpractice
+{
+    class MyCanvas
+    {
+
+        private Bitmap image;
+        private BitmapData bd;
+
+        private int width;
+        private int height;
+        private int bpp;
+
+        public MyCanvas(int w, int h)
+        {
+            width = w;
+            height = h;
+
+            image = new Bitmap(w, h);
+            bd = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            bpp = Bitmap.GetPixelFormatSize(bd.PixelFormat) / 8;
+        }
+
+        public byte[] GetPixel(int x, int y, int bytes)
+        {
+            byte[] color = new byte[bytes];
+
+            unsafe
+            {
+                var firstByte = (byte*)bd.Scan0 + (y * bd.Stride + x * bpp);
+                for (int i = 0; i < bytes; ++i)
+                {
+                    color[i] = firstByte[i];
+                }
+            }
+
+            return color;
+        }
+
+        public void SetPixel(int x, int y, byte[] color)
+        {
+            unsafe
+            {
+                var firstByte = (byte*)bd.Scan0 + (y * bd.Stride + x * bpp);
+                for (int i = 0; i < color.Length; ++i)
+                {
+                    firstByte[i] = color[i];
+                }
+            }
+        }
+
+        public void Save(String fileName)
+        {
+            image.UnlockBits(bd);
+
+            image.Save(fileName, ImageFormat.Bmp);
+        }
+    }
+
+}
---
+++ CGpractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace CGpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // size of canvas is 255 x 255
+            MyCanvas canvas = new MyCanvas(255, 255);
+
+            for (int i = 0; i < 256; ++i)
+            {
+                for (int j = 0; j < 256; ++j)
+                {
+                    double dist = Math.Sqrt((127 - i) * (127 - i) + (127 - j) * (127 - j));
+                    // color of pixel (i, j)
+                    double r = (dist * Math.Sqrt(i * j)) % 255;
+                    double g = 255 - r;
+                    double b = 0;
+
+                    canvas.SetPixel(i, j, new byte[] { (byte) b, (byte) r, (byte) g });
+                }
+            }
+
+            canvas.Save("myImage.bmp");
+        }
+    }
+}
