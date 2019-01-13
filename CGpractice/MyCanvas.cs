f90369c600ccf06d16e79f1e53979c4743e5c1b9
Терещенко Дмитрий <dima123-99@mail.ru>
2019-01-14 00:07:49 +0300

    DrawLine_DrawCircle_FillArea
    Other transparent, rotate, scale circle and rectangle

--- CGpractice/MyCanvas.cs
+++ CGpractice/MyCanvas.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
@@ -59,7 +60,671 @@ namespace CGpractice
             image.UnlockBits(bd);
 
             image.Save(fileName, ImageFormat.Bmp);
-        }
-    }
+		}
 
+		public void DrawLineParam(double x1, double y1, double x2, double y2)
+		{
+			int N = Math.Max(Math.Abs((int)(x2 - x1)), Math.Abs((int)(y2 - y1))) + 1;
+
+			double dt = 1 / (double)(N - 1);
+
+			double vec1 = 1;
+			double vec2 = 6;
+
+			double r = 5;
+			double g = 40;
+			double b = 40;
+
+			double tx = x1;
+			double ty = y1;
+
+			double tv = vec1;
+
+
+			for (int i = 0; i < N; i++)
+			{
+				SetPixel((int)tx, (int)ty, new byte[] { (byte)(r * tv), (byte)(g * tv), (byte)(b * tv) });
+				tx += dt * (x2 - x1);
+				ty += dt * (y2 - y1);
+				tv += dt * (vec2 - vec1);
+			}
+		}
+
+		public void DrawLineDDA(double x1, double y1, double x2, double y2, double r = 200, double g = 200, double b = 20)
+		{
+			double k = (Math.Abs(y2 - y1)) / (double)(Math.Abs(x2 - x1));
+
+			double tx = x1;
+			double ty = y1;
+
+			double dx;
+			double dy;
+			if (k <= 1)
+			{
+				dx = 1;
+				if (x2 - x1 < 0) dx *= -1;
+
+				dy = k;
+				if (y2 - y1 < 0) dy *= -1;
+
+				while ((int)tx != (int)x2)
+				{
+					SetPixel((int)tx, (int)ty, new byte[] { (byte)r, (byte)g, (byte)b });
+					tx += dx;
+					ty += dy;
+				}
+			}
+			else
+			{
+				dx = 1 / k;
+				if (x2 - x1 < 0) dx *= -1;
+
+
+				dy = 1;
+					if (y2 - y1 < 0) dy *= -1;
+
+				while ((int)ty != (int)y2)
+				{
+					SetPixel((int)tx, (int)ty, new byte[] { (byte)r, (byte)g, (byte)b });
+					tx += dx;
+					ty += dy;
+				}
+			}
+		}
+
+		public void DrawLineBrez(double x1, double y1, double x2, double y2)
+		{
+			double r = 100;
+			double g = 200;
+			double b = 20;
+
+			double dy = y2 - y1;
+			double dx = x2 - x1;
+			double d = 2 * dy - dx;
+			double x = x1;
+			double y = y1;
+			do
+			{
+				SetPixel((int)x, (int)y, new byte[] { (byte)r, (byte)g, (byte)b });
+				if (d < 0) d += 2 * dy;
+				else { d += 2 * (dy - dx); ++y; }
+				++x;
+			} while (x <= x2);
+		}
+
+		private void PutCircPixels(int x0, int y0, int x1, int y1, int x2, int y2)
+		{
+			DrawLineDDA(x1, y1, x2, y2); //1 сектор
+			DrawLineDDA(2 * x0 - x1, 2 * y0 - y1, 2 * x0 - x2, 2 * y0 - y2); //Противоположно 1 сектору
+
+			DrawLineDDA(x0 + (y1 - y0), y0 + (x1 - x0), x0 + (y2 - y0), y0 + (x2 - x0)); //2 сектор
+			DrawLineDDA(x0 - (y1 - y0), y0 - (x1 - x0), x0 - (y2 - y0), y0 - (x2 - x0)); //Противоположно 2 сектору
+
+			DrawLineDDA(x0 - (y1 - y0), y0 + (x1 - x0), x0 - (y2 - y0), y0 + (x2 - x0)); //3 сектор
+			DrawLineDDA(x0 + (y1 - y0), y0 - (x1 - x0), x0 + (y2 - y0), y0 - (x2 - x0)); //Противоположно 3 сектору
+
+			DrawLineDDA(2 * x0 - x1, y1, 2 * x0 - x2, y2); //4 сектор
+			DrawLineDDA(x1, 2 * y0 - y1, x2, 2 * y0 - y2); //Противоположно 4 сектору
+		}
+		public void DrawCircleParam(int x0, int y0, int rad)
+		{
+			int phi = 0;
+			int x1, x2 = x0 + rad;
+			int y1, y2 = y0;
+			for (; phi <= 45; ++phi) 
+			{
+				x1 = x2; y1 = y2;
+				x2 = (int)(rad * Math.Cos((phi * Math.PI) / 180)) + x0;
+				y2 = (int)(rad * Math.Sin((phi * Math.PI) / 180)) + y0;
+				PutCircPixels(x0, y0, x1, y1, x2, y2);
+			}
+		}
+
+		private void PutCircPixels(int x0, int y0, int x, int y, double r, double g, double b)
+		{
+			SetPixel(x, y, new byte[] { (byte)r, (byte)g, (byte)b }); //1 сектор
+			SetPixel(2 * x0 - x, 2 * y0 - y, new byte[] { (byte)r, (byte)g, (byte)b }); //Противоположно 1 сектору
+
+			SetPixel(x0 + (y - y0), y0 + (x - x0), new byte[] { (byte)r, (byte)g, (byte)b }); //2 сектор
+			SetPixel(x0 - (y - y0), y0 - (x - x0), new byte[] { (byte)r, (byte)g, (byte)b }); //Противоположно 2 сектору
+
+			SetPixel(x0 - (y - y0), y0 + (x - x0), new byte[] { (byte)r, (byte)g, (byte)b }); //3 сектор
+			SetPixel(x0 + (y - y0), y0 - (x - x0), new byte[] { (byte)r, (byte)g, (byte)b }); //Противоположно 3 сектору
+
+			SetPixel(2 * x0 - x, y, new byte[] { (byte)r, (byte)g, (byte)b }); //4 сектор
+			SetPixel(x, 2 * y0 - y, new byte[] { (byte)r, (byte)g, (byte)b }); //Противоположно 4 сектору
+		}
+
+		public void DrawCircleNeyavnoe(int x0, int y0, int rad)
+		{
+			for (int x = x0; x <= x0 + rad / Math.Sqrt(2); ++x)
+			{
+				int y = (int)(Math.Sqrt(rad * rad - (x - x0) * (x - x0))) + y0;
+				PutCircPixels(x0, y0, x, y, 10, 150, 20);
+			}
+		}
+
+		public void DrawCircleBrez(int x0, int y0, int rad)
+		{
+			int d = 3 - 2 * rad;
+			int x = x0;
+			int y = y0 + rad;
+			do
+			{
+				PutCircPixels(x0, y0, x, y, 50, 60, 100);
+				if (d < 0)
+				{
+					d += 4 * (x - x0) + 6;
+				}
+				else
+				{
+					d += 4 * (x - x0 + y0 - y) + 10; --y;
+				}
+				++x;
+			} while (x < x0 + rad / Math.Sqrt(2));
+		}
+
+		private void FillPixel(int tx, int ty, byte[] color, byte[] baseColor)
+		{
+			var tempColor = GetPixel(tx, ty, 3);
+			if (tempColor[0] == baseColor[0] && tempColor[1] == baseColor[1] && tempColor[2] == baseColor[2])
+			{
+				SetPixel(tx, ty, color);
+				if (tx - 1 >= 0) FillPixel(tx - 1, ty, color, baseColor);
+				if (tx + 1 <= 255) FillPixel(tx + 1, ty, color, baseColor);
+				if (ty - 1 >= 0) FillPixel(tx, ty - 1, color, baseColor);
+				if (ty + 1 <= 255) FillPixel(tx, ty + 1, color, baseColor);
+			}
+		}
+		public void FillOnClickRec(int x0, int y0, double r, double g, double b)
+		{
+			var baseColor = GetPixel(x0, y0, 3);
+			FillPixel(x0, y0, new byte[] { (byte)r, (byte)g, (byte)b }, baseColor);
+		}
+
+		public void FillOnClickIt(int x0, int y0, double r, double g, double b)
+		{
+			var baseColor = GetPixel(x0, y0, 3);
+			var color = new byte[] { (byte)r, (byte)g, (byte)b };	
+			var stack = new Stack<int[]>();
+			stack.Push(new int[] { x0, y0 });
+			do
+			{
+				var tempPixel = stack.Pop();
+				int tx = tempPixel[0];
+				int ty = tempPixel[1];
+				var tempColor = GetPixel(tx, ty, 3);
+				if (tempColor[0] == baseColor[0] && tempColor[1] == baseColor[1] && tempColor[2] == baseColor[2])
+				{
+					SetPixel(tx, ty, color);
+					if (tx - 1 >= 0) stack.Push(new int[] { tx - 1, ty });
+					if (tx + 1 <= 255) stack.Push(new int[] { tx + 1, ty });
+					if (ty - 1 >= 0) stack.Push(new int[] { tx, ty - 1 });
+					if (ty + 1 <= 255) stack.Push(new int[] { tx, ty + 1 });
+				}
+			} while (stack.Count != 0);
+
+		}
+	}
+
+	class Vector2
+	{
+		public double x;
+		public double y;
+		public Vector2(double _x, double _y)
+		{
+			x = _x;
+			y = _y;
+		}
+
+		public static Vector2 operator +(Vector2 vec1, Vector2 vec2)
+		{
+			return new Vector2(vec1.x + vec2.x, vec1.y + vec2.y);
+		}
+
+		public static Vector2 operator -(Vector2 vec1, Vector2 vec2)
+		{
+			return new Vector2(vec1.x - vec2.x, vec1.y - vec2.y);
+		}
+
+		public static Vector2 operator *(Vector2 vec1, double m)
+		{
+			return new Vector2(vec1.x * m, vec1.y * m);
+		}
+
+		public static double operator *(Vector2 vec1, Vector2 vec2)
+		{
+			return vec1.x * vec2.x + vec1.y * vec2.y;
+		}
+
+		public static Vector2 operator /(Vector2 vec1, double m)
+		{
+			return new Vector2(vec1.x / m, vec1.y / m);
+		}
+
+		public static double Distance(Vector2 vec1, Vector2 vec2)
+		{
+			return (double)Math.Sqrt(Math.Pow(vec1.x - vec2.x, 2) + Math.Pow(vec1.y - vec2.y, 2));
+		}
+
+		public double Length()
+		{
+			return (double)Math.Sqrt(x * x + y * y);
+		}
+
+		public static double Dot(Vector2 vec1, Vector2 vec2) {
+			return (vec1.x * vec2.x + vec1.y * vec2.y);
+		}
+
+		public static double Cross(Vector2 vec1, Vector2 vec2)
+		{
+			return -(vec1.x * vec2.y - vec1.y * vec2.x);
+		}
+
+		public static double Angle(Vector2 vec1, Vector2 vec2)
+		{
+
+			double angle = Math.Acos(Vector2.Dot(vec1, vec2) / (vec1.Length() * vec2.Length()));
+			//if (Vector2.Cross(vec1, vec2) >= 0)
+			//{
+			//	return angle;
+			//}
+			//else
+			//{
+			//	return Math.PI * 2 - angle;
+			//}
+			return angle;
+		}
+	}
+
+	class Nangle
+	{
+		private MyCanvas canvas;
+		private Vector2[] points;
+		private Vector2 pArea1;
+		private Vector2 pArea2;
+		private void CountArea()
+		{
+			pArea1 = new Vector2(256, 256);
+			pArea2 = new Vector2(0, 0);
+			for (int i = 0; i < points.Length; i++)
+			{
+				if (points[i].x < pArea1.x) pArea1.x = points[i].x;
+				if (points[i].y < pArea1.y) pArea1.y = points[i].y;
+				if (points[i].x > pArea2.x) pArea2.x = points[i].x;
+				if (points[i].y > pArea2.y) pArea2.y = points[i].y;
+			}
+			//canvas.DrawLineDDA(pArea1.x, pArea1.y, pArea1.x, pArea2.y, 150, 10, 100);
+			//canvas.DrawLineDDA(pArea1.x, pArea1.y, pArea2.x, pArea1.y, 150, 10, 100);
+			//canvas.DrawLineDDA(pArea2.x, pArea2.y, pArea2.x, pArea1.y, 150, 10, 100);
+			//canvas.DrawLineDDA(pArea2.x, pArea2.y, pArea1.x, pArea2.y, 150, 10, 100);
+		}
+
+		public Nangle(Vector2[] _points, MyCanvas _canvas)
+		{
+			points = _points;
+			canvas = _canvas;
+			for (int i = 1; i < points.Length; i++)
+			{
+				canvas.DrawLineDDA(points[i - 1].x, points[i - 1].y, points[i].x, points[i].y);
+			}
+			canvas.DrawLineDDA(points[points.Length - 1].x, points[points.Length - 1].y, points[0].x, points[0].y);
+			CountArea();
+		}
+
+		public void FillForAngle()
+		{
+			double r = 30;
+			double g = 100;
+			double b = 70;
+
+			for (double y = pArea1.y; y < pArea2.y; y++)
+			{
+				for (double x = pArea1.x; x < pArea2.x; x++)
+				{
+					double angle = 0.0;
+					Vector2 vec1;
+					Vector2 vec2;
+					for (int i = 0; i < points.Length - 1; i++)
+					{
+						vec1 = new Vector2(points[i].x - x, points[i].y - y);
+						vec2 = new Vector2(points[i + 1].x - x, points[i + 1].y - y);
+						angle += (Vector2.Angle(vec2, vec1) * 180) / Math.PI;
+					}
+					vec1 = new Vector2(points[points.Length - 1].x - x, points[points.Length - 1].y - y);
+					vec2 = new Vector2(points[0].x - x, points[0].y - y);
+					angle += (Vector2.Angle(vec2, vec1) * 180) / Math.PI;
+					if (Math.Abs(angle - 360) <= 0.05)
+					{
+						canvas.SetPixel((int)x, (int)y, new byte[] { (byte)r, (byte)g, (byte)b });
+					}
+				}
+			}
+		}
+
+		public void FillForRow()
+		{
+			/*Улучшение: 
+			 * 1.Составить список ребёр, чтобы y1 < y2.
+			 * 2.Спускаться от yMin до yMax. Если есть рёбра, у которых начало совпадает с текущим y, добавить их в CAP.
+			 * 3.Рёбра в САР должны быть упорядочены по x.
+			 * 4.С рёбрами из САР найти иксы по текущему y и заполнить получившиеся отрезки
+			 * 5.Если какое-то ребро вышло из границ рассмотрения, то удалить его из САР
+			 */
+			double r = 100;
+			double g = 50;
+			double b = 50;
+
+			
+			var ords = new List<double>[(int)(pArea2.y - pArea1.y) + 1];
+			for (int i = 0; i < ords.Length; i++)
+			{
+				ords[i] = new List<double>();
+			}
+
+			double x;
+			double y;
+			double dx;
+			for (int i = 0; i < points.Length; i++)
+			{
+				double x1 = points[i].x;
+				double y1 = points[i].y;
+				double x2, y2;
+				if (i != points.Length - 1)
+				{
+					x2 = points[i + 1].x;
+					y2 = points[i + 1].y;
+				}
+				else
+				{
+					x2 = points[0].x;
+					y2 = points[0].y;
+				}
+				if (y2 < y1)
+				{
+					double t = y2;
+					y2 = y1;
+					y1 = t;
+
+					t = x2;
+					x2 = x1;
+					x1 = t;
+				}
+				x = x1;
+				y = y1;
+				dx = (x2 - x1) / (y2 - y1);
+				while ((int)y != (int)y2)
+				{
+					ords[(int)(y - pArea1.y)].Add(x);
+					++y; ; x += dx;
+				}
+			}
+
+			for (int ty = 0; ty < ords.Length; ty++)
+			{
+				if (ords[ty].Count >= 2)
+				{
+					for (int i = 0; i <= ords[ty].Count - 2; i += 2)
+					{
+						canvas.DrawLineDDA(ords[ty][i], ty + pArea1.y, ords[ty][i + 1], ty + pArea1.y, r, g, b);
+					}
+				}
+			}
+		}
+	}
+
+	class MyCircle
+	{
+		private MyCanvas canvas;
+
+		private double r = 30;
+		private double g = 128;
+		private double b = 255;
+
+		private int x;
+		private int y;
+		private int rad;
+
+		public MyCircle(int _x, int _y, int _rad, MyCanvas _canvas)
+		{
+			x = _x;
+			y = _y;
+			rad = _rad;
+			canvas = _canvas;
+			for (int i = y - rad; i < y + rad; ++i)
+			{
+				for (int j = x - rad; j < x + rad; ++j)
+				{
+					if ((i - x) * (i - x) + (j - y) * (j - y) <= rad * rad)
+					{
+						canvas.SetPixel(i, j, new byte[] { (byte)r, (byte)g, (byte)b });
+					}
+				}
+			}
+		}
+
+		public void Scale(int cx, int cy, double a)
+		{
+			for (int i = y - rad; i < y + rad; ++i)
+			{
+				for (int j = x - rad; j < x + rad; ++j)
+				{
+					if ((i - x) * (i - x) + (j - y) * (j - y) <= rad * rad)
+					{
+						int tx = (int)(cx + (i - cx) * a);
+						int ty = (int)(cy + (j - cy) * a);
+						if (tx >= 0 && tx <= 255 && ty >= 0 && ty <= 255)
+						{
+							var color = canvas.GetPixel(i, j, 3);
+							canvas.SetPixel(tx, ty, color);
+						}
+					}
+				}
+			}
+
+		}
+
+		public void NewScale(int cx, int cy, double a)
+		{
+			double errorx = 0.0;
+			double errory = 0.0;
+			for (int i = 0; i < 256; ++i)
+			{
+				for (int j = 0; j < 256; ++j)
+				{
+					int tx = (int)((i + cx * (a - 1)) / a);
+					int ty = (int)((j + cy * (a - 1)) / a);
+
+					errorx += (((i + cx * (a - 1)) / a) - tx);
+					errory += (((j + cy * (a - 1)) / a) - ty);
+
+					int dx = 0;
+					int dy = 0;
+					if (errorx >= 1.0) { errorx = 0.0; dx = 1; }
+					if (errory >= 1.0) { errory = 0.0; dy = 1; }
+					if ((tx - x) * (tx - x) + (ty - y) * (ty - y) <= rad * rad)
+					{
+						var color = canvas.GetPixel(tx, ty, 3);
+						canvas.SetPixel(i, j, color);
+						canvas.SetPixel(i + dx, j + dy, color);
+					}
+				}
+			}
+		}
+
+		public void Transform(int dx, int dy)
+		{
+			for (int i = y - rad; i < y + rad; ++i)
+			{
+				for (int j = x - rad; j < x + rad; ++j)
+				{
+					if ((i - x) * (i - x) + (j - y) * (j - y) <= rad * rad)
+					{
+						int tx = i + dx;
+						int ty = j + dy;
+						if (tx >= 0 && tx <= 255 && ty >= 0 && ty <= 255)
+						{
+							var color = canvas.GetPixel(i, j, 3);
+							canvas.SetPixel(tx, ty, color);
+						}
+					}
+				}
+			}
+		}
+
+		public void Rotate(int cx, int cy, double _angle)
+		{
+			double angle = (_angle * Math.PI) / 180;
+			for (int i = y - rad; i < y + rad; ++i)
+			{
+				for (int j = x - rad; j < x + rad; ++j)
+				{
+					if ((i - x) * (i - x) + (j - y) * (j - y) <= rad * rad)
+					{
+						int tx = (int)((i - cx) * Math.Cos(angle) - (j - cy) * Math.Sin(angle)) + cx;
+						int ty = (int)((i - cx) * Math.Sin(angle) + (j - cy) * Math.Cos(angle)) + cy;
+						if (tx >= 0 && tx <= 255 && ty >= 0 && ty <= 255)
+						{
+							var color = canvas.GetPixel(i, j, 3);
+							canvas.SetPixel(tx, ty, color);
+						}
+					}
+				}
+			}
+
+		}
+
+	}
+
+	class MyRectangle
+	{
+		private MyCanvas canvas;
+
+		private double r = 30;
+		private double g = 128;
+		private double b = 255;
+
+		private int x;
+		private int y;
+		private int w;
+		private int h;
+
+		public MyRectangle(int _x, int _y, int _w, int _h, MyCanvas _canvas)
+		{
+			x = _x;
+			y = _y;
+			w = _w;
+			h = _h;
+			canvas = _canvas;
+			for (int i = y; i < y + h; ++i)
+			{
+				for (int j = x; j < x + w; ++j)
+				{
+					canvas.SetPixel(i, j, new byte[] { (byte)r, (byte)g, (byte)b });
+				}
+			}
+		}
+
+		public void Scale(int cx, int cy, double a)
+		{
+			for (int i = y; i < y + h; ++i)
+			{
+				for (int j = x; j < x + w; ++j)
+				{
+					int tx = (int)(cx + (i - cx) * a);
+					int ty = (int)(cy + (j - cy) * a);
+					if (tx >= 0 && tx <= 255 && ty >= 0 && ty <= 255)
+					{
+						var color = canvas.GetPixel(i, j, 3);
+						canvas.SetPixel(tx, ty, color);
+					}
+				}
+			}
+		}
+
+		public void NewScale(int cx, int cy, double a)
+		{
+			double errorx = 0.0;
+			double errory = 0.0;
+			for (int i = 0; i < 256; ++i)
+			{
+				for (int j = 0; j < 256; ++j)
+				{
+					int tx = (int)((i + cx * (a - 1)) / a);
+					int ty = (int)((j + cy * (a - 1)) / a);
+					
+
+					errorx += (((i + cx * (a - 1)) / a) - tx);
+					errory += (((j + cy * (a - 1)) / a) - ty);
+
+					int dx = 0;
+					int dy = 0;
+					if (errorx >= 1.0) { errorx = 0.0; dx = 1; }
+					if (errory >= 1.0) { errory = 0.0; dy = 1; }
+
+					if (tx >= x && tx <= x + w && ty >= y && ty <= y + h)
+					{
+						var color = canvas.GetPixel(tx, ty, 3);
+						canvas.SetPixel(i, j, color);
+						canvas.SetPixel(i + dx, j + dy, color);
+					}
+				}
+			}
+		}
+
+		public void Transform(int dx, int dy)
+		{
+			for (int i = y; i < y + h; ++i)
+			{
+				for (int j = x; j < x + w; ++j)
+				{
+					int tx = i + dx;
+					int ty = j + dy;
+					if (tx >= 0 && tx <= 255 && ty >= 0 && ty <= 255)
+					{
+						var color = canvas.GetPixel(i, j, 3);
+						canvas.SetPixel(tx, ty, color);
+					}
+				}
+			}
+		}
+
+		public void Rotate(int cx, int cy, double _angle)
+		{
+			double angle = (_angle * Math.PI) / 180;	
+			for (int i = y; i < y + h; ++i)
+			{
+				for (int j = x; j < x + w; ++j)
+				{
+					int tx = (int)((i - cx) * Math.Cos(angle) - (j - cy) * Math.Sin(angle)) + cx;
+					int ty = (int)((i - cx) * Math.Sin(angle) + (j - cy) * Math.Cos(angle)) + cy;
+					if (tx >= 0 && tx <= 255 && ty >= 0 && ty <= 255)
+					{
+						var color = canvas.GetPixel(i, j, 3);
+						canvas.SetPixel(tx, ty, color);
+					}
+				}
+			}
+		}
+
+		public void NewRatate(int cx, int cy, double _angle)
+		{
+			double angle = (_angle * Math.PI) / 180;
+			for (int i = 0; i < 256; ++i)
+			{	
+				for (int j = 0; j < 256; ++j)
+				{
+					int tx = (int)((i - (cx - (cx) * Math.Cos(angle) + (cy) * Math.Sin(angle))) * Math.Cos(angle)
+						+ (j - (cy - (cx) * Math.Sin(angle) - (cy) * Math.Cos(angle))) * Math.Sin(angle));
+					int ty = (int)( -1	* (i - (cx - (cx) * Math.Cos(angle) + (cy) * Math.Sin(angle))) * Math.Sin(angle) 
+						+ (j - (cy - (cx) * Math.Sin(angle) - (cy) * Math.Cos(angle))) * Math.Cos(angle));
+					if (tx >= x && tx <= x + w && ty >= y && ty <= y + h)
+					{
+						var color = canvas.GetPixel(tx, ty, 3);
+						canvas.SetPixel(i, j, color);
+					}
+				}
+			}
+		}
+	}
 }
--- CGpractice/Program.cs
+++ CGpractice/Program.cs
@@ -9,22 +9,82 @@ namespace CGpractice
         {
             // size of canvas is 255 x 255
             MyCanvas canvas = new MyCanvas(255, 255);
+			//for (int i = 0; i < 256; ++i)
+			//{
+			//	for (int j = 0; j < 256; ++j)
+			//	{
+			//		double dist = Math.Sqrt((127 - i) * (127 - i) + (127 - j) * (127 - j));
+			//		double r = (dist * Math.Sqrt(i * j)) % 255;
+			//		double g = 255 - r;
+			//		double b = 0;
+			//		canvas.SetPixel(i, j, new byte[] { (byte)r, (byte)g, (byte)b });
+			//	}
+			//}
 
-            for (int i = 0; i < 256; ++i)
-            {
-                for (int j = 0; j < 256; ++j)
-                {
-                    double dist = Math.Sqrt((127 - i) * (127 - i) + (127 - j) * (127 - j));
-                    // color of pixel (i, j)
-                    double r = (dist * Math.Sqrt(i * j)) % 255;
-                    double g = 255 - r;
-                    double b = 0;
+			//int x1 = 75;
+			//int y1 = 75;
+			//int x2 = 125;
+			//int y2 = 125;
 
-                    canvas.SetPixel(i, j, new byte[] { (byte) b, (byte) r, (byte) g });
-                }
-            }
+			int d = 100;
+			int c = 100;
+			//var cir = new MyCircle(c, c, 25, canvas);
+			//cir.Scale(c, c, 2f);
+			//cir.NewScale(c, c, 2f);
+			//cir.Transform(50, 50);
+			//cir.Rotate(c - 25, c - 25, 120);
+			//var rec = new MyRectangle(c, c, 50, 50, canvas);
+			//rec.Scale(c + 25, c + 25, 2);
+			//rec.NewScale(c + 25, c + 25, 3);
+			//rec.Transform(50, 50);
+			//rec.Rotate(0, 0, 30);
+			//rec.NewRatate(c, c, 120);
+			double x1 = 11.33;
+			double y1 = 109.5;
 
-            canvas.Save("myImage.bmp");
-        }
+			double x2 = 104.44;
+			double y2 = 137.6;
+
+			double step = 10;
+			//canvas.DrawLineParam(x1, y1, x2, y2);
+			//canvas.DrawLineDDA(x1, y1 + step, x2, y2  + step);
+			//canvas.DrawLineBrez(x1, y1 + 2 * step, x2, y2 + 2 * step);
+
+			//canvas.DrawCircleParam(50, 100, 30);
+			//canvas.DrawCircleNeyavnoe(100, 100, 30);
+			//canvas.DrawCircleBrez(150, 100, 30);
+
+			//Console.Write("Input x2: ");
+			//x2 = Double.Parse(Console.ReadLine());
+			//Console.Write("Input y2: ");
+			//y2 = Double.Parse(Console.ReadLine());
+			//canvas.DrawLineParam(x1, y1, x2, y2);
+			//canvas.DrawLineDDA(x1, y1 + step, x2, y2 + step);
+			//canvas.DrawLineBrez(x1, y1 + 2 * step, x2, y2 + 2 * step);
+
+			Vector2[] points = new Vector2[6];
+			points[0] = new Vector2(50, 100);
+			points[1] = new Vector2(50, 50);
+			points[2] = new Vector2(120, 90);
+			points[3] = new Vector2(180, 60);
+			points[4] = new Vector2(200, 150);
+			points[5] = new Vector2(100, 150);
+			Nangle nangle = new Nangle(points, canvas);
+			//nangle.FillForAngle();
+			//nangle.FillForRow();
+
+			int xClilk = 190;
+			int yClick = 21;
+			//canvas.FillOnClickRec(xClilk, yClick, 100, 150, 100);
+			canvas.FillOnClickIt(xClilk, yClick, 100, 150, 100);
+			//Vector2[] points1 = new Vector2[4];
+			//points1[0] = new Vector2(50, 200);
+			//points1[1] = new Vector2(100, 200);
+			//points1[2] = new Vector2(100, 250);
+			//points1[3] = new Vector2(50, 250);
+			//Nangle nangle1 = new Nangle(points1, canvas);
+			//nangle1.FillForAngle();
+			canvas.Save("myImage.bmp");
+		}
     }
 }
